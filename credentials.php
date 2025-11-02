8e2a79cf6c1b5f723b575a72cc47cac7fe401063
Simon Otto <otto0237@umn.edu>
2025-11-02 17:08:36 +0000

    Fifth Commit

--- credentials.php
+++ credentials.php
@@ -1,6 +1,6 @@
 <?php
 $servername = 'localhost';     //if apache on diff server from db or using Docker, set IP address
 $dbname = 'employees';              //which db you're going to use
-$username = '';
-$password = '';
+$username = 'otto0237';
+$password = 'WAikiki*8802';
 ?>
--- testdbphp.php
+++ testdbphp.php
@@ -1,55 +1,31 @@
-
-<!DOCTYPE html>
+<!doctype html>
 <html>
 <head>
-	<title>Northland Analytics PHP/MySQL Test Page</title>
+<meta charset="utf-8">
+<title>FASSH Tech</title>
 </head>
+<!-- Body -->
 <body>
+        <h2>Employee Management System v1.0.10</h2>
+        <p>This system uses Apache, PHP, and the MariaDB (MySQL) Database System</p>
+        <p>Please Select from the Options Below:</p>
+        <br>
+        <a href="findemployee.html" title="Find Employee" target="_parent">Search Employee Records</a>
+        <br><br>
+        <a href="addemployee.html" title="Add Employee" target="_parent">Add Employee Record</a>
+        <br><br>
+        <a href="updateemployee.html" title="Update Employee" target="_parent">Update Employee Record</a>
+        <br><br>
+    <a href="deleteemployee.html" title="Delete Employee" target="_parent">Delete Employee Record</a>
+        <br><br>
+        <a href="findemployeeajax2.html" title="Find Employee with AJAX" target="_parent">Find Employee Record AJAX</a>
+        <br><br>
+<p>Simple Client-Side JavaScript Example - Click to show the date</p>
+<p>This code is ran by the broswer - a browser runs the JavaScript.  Just one line of code.</p>
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
 
-	<h1>Northland Analytics PHP/MySQL Test Page </h1>
-	
-	<p>Database Records Found</p>
-<?php
-//this is the php object oriented style of creating a mysql connection
-$conn = new mysqli('localhost', 'joeax', 'abc123', 'employees' );
-
-//check for connection success
-if ($conn->connect_error) {
-	die("MySQL Connection Failed: " . $conn->connect_error);
-   }
-echo "MySQL Connection Succeeded<br><br>";
-
-//create the SQL select statement, notice the funky string concat at the end to variablize the query
-//based on using the GET attribute
-$sql = "SELECT first_name,last_name FROM employees where last_name = 'Weedman'";
-
-//put the resultset into a variable, again object oriented way of doing things here
-$result = $conn->query($sql);
-
-//if there were no records found say so, otherwise create a while loop that loops through all rows
-//and echos each line to the screen. You do this by creating some crazy looking echo statements
-// in the form of HTMLText . row[column] . HTMLText . row[column].   etc...
-// the dot "." is PHP's string concatenator operator
-if ($result->num_rows > 0){
-//print rows of records found in the database if any
-    while($row = $result->fetch_assoc()){
-         echo "Employee: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
-   }
- } else {
-         echo "No Records Found";
- }
-
-//always close the connection to the DB, don't leave 'em hanging
-$conn->close();
-
-?>
-
-<br><br>
-<p>
-	For more tips and trick to connect PHP to MySQL,
-	<a href="https://www.php.net/manual/en/book.mysqli.php" target="_blank">
-	click here
-	</a>.
-	</p>
+<p id="demo"></p>
 </body>
 </html>
